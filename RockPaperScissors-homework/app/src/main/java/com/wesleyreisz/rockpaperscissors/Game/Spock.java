9b9fa72501fe8c84d06475e5406424b6e5061a62
NickMattmiller <njmatt92@gmail.com>
2015-09-28 15:15:39 -0400

    Rock Paper Scissors Lizard Spock first commit
    added logic, basic pics, game types,

--- RockPaperScissors-homework/app/src/main/java/com/wesleyreisz/rockpaperscissors/Game/Spock.java
+++ RockPaperScissors-homework/app/src/main/java/com/wesleyreisz/rockpaperscissors/Game/Spock.java
@@ -1,28 +1,27 @@
 package com.wesleyreisz.rockpaperscissors.Game;
 
+/**
+ * Created by Nick on 9/28/2015.
+ */
+
 import com.wesleyreisz.rockpaperscissors.GameUtils;
 import com.wesleyreisz.rockpaperscissors.R;
 
-/**
- * Created by wesleyreisz on 9/13/15.
- */
-public class Scissors implements GameType {
+public class Spock implements GameType {
 
-    @Override
-    public String eval(Integer opponentChoice) {
-        if (opponentChoice== R.id.btnPaper){
+    public String eval(Integer opponentChoice)  {
+        if (opponentChoice== R.id.btnScissors) {
             return GameUtils.BEATS;
-        }else if (opponentChoice==R.id.btnLizard){
+        }else if (opponentChoice==R.id.btnRock){
             return GameUtils.BEATS;
-        }
-        else if (opponentChoice==R.id.btnRock){
+        }else if (opponentChoice==R.id.btnPaper){
             return GameUtils.LOSES_TO;
-        }
-        else if (opponentChoice==R.id.btnSpock){
+        }else if (opponentChoice==R.id.btnLizard){
             return GameUtils.LOSES_TO;
         }
         else{
             return GameUtils.TIES;
         }
     }
+
 }
